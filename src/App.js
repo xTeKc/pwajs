7be148e44005927818dd5d2927769d02e1555305
xTeKc <bcd3v@pm.me>
2021-09-25 23:11:55 -0400

    fill value and set query

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import { fetchData } from './api/fetchData';
 import './App.css';
 
 const App = () => {
+    const [query, setQuery] = useState('');
+
     return (
         <div className="main-container">
             <input
                 type="text"
                 className="search"
                 placeholder="Search..."
-                value={}
-                onChange={}
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
             />
         </div>
     );
