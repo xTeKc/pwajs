489f9a94d6748469272bb89e9a42354476e00315
xTeKc <bcd3v@pm.me>
2021-09-25 22:56:32 -0400

    setup data

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -1,7 +1,16 @@
 import axios from 'axios';
 
-const URL = '';
+const URL = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd';
 
-const fetchData = async (query) => {
-    const response = await axios.get(URL);
+export const fetchData = async (query) => {
+    const { data } = await axios.get(URL, {
+        params: {
+            q: query,
+            cName: 'name',
+            cSymbol: 'symbol',
+            cContract: 'contract'
+        }
+    });
+
+    return data;
 }
